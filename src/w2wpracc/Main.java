b62d0258817f639a34a44a0f5d00719b94844410
gollgot <loic.dessaules@cpnv.ch>
2016-09-16 09:07:49 +0200

    Classe ParsingJSON + modifs Main
    J'ai créé une classe ParsinJSON et depuis le Main j'arrive à récupérer
    toutes les infos du JSON et les stocker dans une variable

--- src/w2wpracc/Main.java
+++ src/w2wpracc/Main.java
@@ -5,11 +5,16 @@
  */
 package w2wpracc;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -40,6 +45,23 @@ public class Main extends Application {
         }else{
             System.out.println("The cache file already exists.");
         }
+        
+        /* Move later (get Json) */
+        try {
+            JSONObject json = ParsingJSON.readJsonFromUrl("http://www.omdbapi.com/?t=titanic&y=&plot=full&r=json");
+            String title = json.get("Title").toString();
+            String director = json.get("Director").toString();
+            String actors = json.get("Actors").toString();
+            String year = json.get("Year").toString();
+            String poster = json.get("Poster").toString();
+            String genre = json.get("Genre").toString();
+        } catch (JSONException ex) {
+            System.out.println("ERROR on parsingJSON (JSON exception) : "+ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("ERROR on parsingJSON (IO exception) : "+ex.getMessage() + "\nVeuillez vérifier votre connexion internet");
+        }
+        
+                
         
         /* After : Launch the window */
         launch(args);  
